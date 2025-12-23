297871e7d98618e96a6e44e1f7bb50cb02d01d95
Nikhil Kumar S <nikhilkumars12343@gmail.com>
2025-12-23 10:08:21 +0530

    All service added

---
+++ src/app/our-services/video-production/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Videoproductionbanner from "@/app/components/page components/video production/Videoproductionbanner"
+import Videoproductioncontent from "@/app/components/page components/video production/Videoproductioncontent"
+
+const page = () => {
+  return (
+    <>
+        <Videoproductionbanner />
+        <Videoproductioncontent />
+    </>
+  )
+}
+
+export default page
